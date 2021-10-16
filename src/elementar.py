5f47f25e1a6394e8d65c81ea4403ff7eae33a790
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-10-16 16:17:23 -0300

    add endpoint soma e subtracao + update requirements

---
+++ src/elementar.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/soma', methods=['POST'])
+def soma():
+  n1 = float(request.form.get('n1'))
+  n2 = float(request.form.get('n2'))
+  return str(n1 + n2)
+
+@app.route('/subt', methods=['POST'])
+def subt():
+  n1 = float(request.form.get('n1'))
+  n2 = float(request.form.get('n2'))
+  return str(n1 - n2)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5002)
